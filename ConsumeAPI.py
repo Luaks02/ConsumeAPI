210dfde66387fb332141f0c819a7c6d710659ed9
Lucas Martins <Lucascamilo02@gmail.com>
2023-01-04 11:57:51 -0300

    Selenium brings the service's material description separated by lines and print in the excel

--- ConsumeAPI.py
+++ ConsumeAPI.py
@@ -1,16 +1,17 @@
-#from openpyxl import workbook, load_workbook
-#import pandas as pd
+from openpyxl import workbook, load_workbook
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+
+#Colhendo informações com Selenium
 
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
 
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.aldo.com.br/categoria/energia-solar?filtro=143")
+driver.get("https://www.aldo.com.br/categoria/energia-solar?filtro=131;3:5~30000")
 
 driver.implicitly_wait(5)
 
@@ -19,20 +20,24 @@ rowshoplist.click()
 driver.implicitly_wait(5)
 description = driver.find_elements(By.XPATH, "//div[@id='description']")[0].text
 
-print(description)
+listed_description = []
 
+listed_description = description.splitlines()
 
+#Enviando informações para a planilha principal
 
+wb = load_workbook(filename="Smart_Haus.xlsm", read_only=False,keep_vba=True)
+ws = wb["Dados_Tecnico-Projeto"]
+
+numero = 2
+
+for linha in listed_description:
+    ws["B" + str(numero)].value = linha
+    numero += 1
+
+#Fechando sistemas
+
+wb.save("Smart_Haus.xlsm")
 driver.quit()
-
-#Colhendo informações
-
-
-#wb = load_workbook(filename="Smart_Haus.xlsm", read_only=False,keep_vba=True)
-#ws = wb["Dados_Tecnico-Projeto"]
-
-#ws["A1"].value = "Teste"
-
-#wb.save("Smart_Haus.xlsm")
 
 
