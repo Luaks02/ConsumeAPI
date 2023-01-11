e5920ac5b12c438130bb551cea00ca55adb8a4d3
Lucas Martins <Lucascamilo02@gmail.com>
2023-01-11 16:55:28 -0300

    Now the code brings the painels information too

--- ConsumeAPI.py
+++ ConsumeAPI.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 #Abrindo a planilha de controle
 wb = load_workbook(filename="Smart_Haus.xlsm", read_only=False,keep_vba=True)
 ws = wb["Dados_Tecnico-Projeto"]
+ws2 = wb["Dados_Proposta"]
 
 site = ws["B1"].value
 
@@ -34,14 +35,17 @@ listed_description = []
 listed_description = description.splitlines()
 
 for index,desc in enumerate(listed_description):
+    if desc[0:4] == "Área":
+        area = desc[25:28]
+    if desc[0:10] == "Peso sobre":
+        peso = desc[23:25]
     if desc[0:36] == "O gerador de energia fotovoltaico de":
         inicio = index+1
-        break
-
-for index,desc in enumerate(listed_description):
-    if desc[0:14] == "Regulamentação":
+    if desc[0:4] == "ESTE":
         fim = index-2
         break
+    if desc[0:14] == "Regulamentação":
+        fim = index-2
 
 #Enviando informações para a planilha de controle
 
@@ -54,12 +58,42 @@ for linha in range(inicio,fim):
     material = listed_description[linha].split()
     ws["B" + str(numero)].value = " ".join(material[1:])
     ws["C" + str(numero)].value = int(material[0])
+    if material[1] == "INVERSOR" or material[1] == "MICROINVERSOR":
+        ws2["H29"].value = material[3]
+        ws2["H31"].value = int(material[0])
+        if material[3] == "GROWATT":
+            ws2["I29"].value = material[6]
+            try:
+                ws2["I31"].value = int(material[11][0])
+            except:
+                ws2["I31"].value = int(material[10][0])
+            ws2["J29"].value = material[8]
+        elif material[3] == "FRONIUS":
+            ws2["I29"].value = " ".join(material[4:6])
+            ws2["I31"].value = int(material[9])
+            ws2["J29"].value = material[6]
+        elif material[3] == "DEYE":
+            ws2["I29"].value = (material[4])
+            ws2["I31"].value = int(material[8][0])
+            ws2["J29"].value = material[5] 
+    if material[1] == "PAINEL":
+        ws2["H25"].value = material[3]
+        ws2["H27"].value = int(material[0])
+        if material[3] == "JINKO":
+            ws2["I25"].value = material[4]
+            ws2["J25"].value = material[7]
+        elif material[3] == "JA":
+            ws2["I25"].value = material[4]
+            ws2["J25"].value = material[5]
+
     numero += 1
 
 ws["B" + str(numero)].value = "Instalação"
 ws["C" + str(numero)].value = 1
 
 #Fechando sistemas
+
+print(area,peso)
 
 wb.save("Smart_Haus.xlsm")
 driver.quit()
